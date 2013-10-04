7108c66e73d96e30b4a5bc5b06f10978557b41a7
mirhagk <mirhagk@gmail.com>
2013-10-04 12:23:36 -0400

    Filled out resume entry

--- website/js/resume.js
+++ website/js/resume.js
@@ -9,8 +9,8 @@ var experience = [{
         {name: 'ExtJS'},
         {name: 'LINQ\SQL'},
         {name: 'ASP.NET'},
-        {name: 'TFS'}
-        
+        {name: 'TFS'},
+        {name: 'RDL/RDLC'}
         ],
     respon: [
         {text: 'Develop websites and design databases'},
@@ -19,41 +19,10 @@ var experience = [{
         {text:'Create reports and database views'},
         {text:'Help new students get started at the work place'}
         ],
-    achiev: [
-        {text: ''}
+    accom: [
+        {text: 'Main developer for several projects including SAHARA clinical trial'},
+        {text: 'Built public website for assessing heart risk score https://rome.phri.ca/interheartriskscore'},
+        {text: 'Found security flaws, such as SQL injections, fixed flaws and led redeployment of project'},
+        {text: 'Assessed new technologies and alternatives to existing systems (such as mobile and SMS service)'}
         ]
 }];
-
-/*
-\cventry{February 2012--December 2012}{Software Solutions Developer}{Population Health Research Institute}{Hamilton}{}{Develop clinical trial management systems.
-\begin{itemize}%
-	\item \textbf{Responsibilities:}
-	\begin{itemize}
-		\item Develop websites and design databases
-		\item Diagnose and fix bugs, documenting solutions using TFS
-		\item Follow specification to create validated systems that comply to the user's needs
-		\item Create reports and database views
-		\item Help get new students get started at the work place
-	\end{itemize}
-	\item \textbf{Main Technologies Used:}
-	\vspace{-4mm} 
-	\begin{multicols}{2}
-	   \begin{itemize}
-		\item \CSharp
-		\item ExtJS
-		\item LINQ\textbackslash SQL
-		\item ASP.NET
-		\item TFS
-		\item RDL/RDLC
-	   \end{itemize}
-	\end{multicols}
-	 \vspace{-3mm} 
-\item \textbf{Accomplishments:}
-  \begin{itemize}
-  \item Main developer for several projects including SAHARA clinical trial
-  \item Built public website for assessing heart risk score \url{https://rome.phri.ca/interheartriskscore}
-  \item Found security flaws, such as SQL injections, fixed flaws and led redeployment of project
-  \item Assessed new technologies and alternatives to existing systems (such as mobile and SMS service)
-  \end{itemize}
-\end{itemize}}
-*/
