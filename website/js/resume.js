830ccbb0365a2b3cb4d5c201c0c3b898c68ed11d
mirhagk <mirhagk@gmail.com>
2013-10-04 10:35:08 -0400

    moved website to website subdirectory

---
+++ website/js/resume.js
@@ -0,0 +1,59 @@
+var experience = [{
+    name: 'Population Health Research Institute',
+    description: 'Develop clinical trial management systems',
+    location: 'Hamilton, ON',
+    dates: {start: 'February 2012', end: 'December 2012'},
+    title: 'Software Solutions Developer',
+    tech: [
+        {name: 'C#'},
+        {name: 'ExtJS'},
+        {name: 'LINQ\SQL'},
+        {name: 'ASP.NET'},
+        {name: 'TFS'}
+        
+        ],
+    respon: [
+        {text: 'Develop websites and design databases'},
+        {text:'Diagnose and fix bugs, documenting solutions using TFS'},
+        {text:"Follow specification to create validated systems that comply to the user's needs"},
+        {text:'Create reports and database views'},
+        {text:'Help new students get started at the work place'}
+        ],
+    achiev: [
+        {text: ''}
+        ]
+}];
+
+/*
+\cventry{February 2012--December 2012}{Software Solutions Developer}{Population Health Research Institute}{Hamilton}{}{Develop clinical trial management systems.
+\begin{itemize}%
+	\item \textbf{Responsibilities:}
+	\begin{itemize}
+		\item Develop websites and design databases
+		\item Diagnose and fix bugs, documenting solutions using TFS
+		\item Follow specification to create validated systems that comply to the user's needs
+		\item Create reports and database views
+		\item Help get new students get started at the work place
+	\end{itemize}
+	\item \textbf{Main Technologies Used:}
+	\vspace{-4mm} 
+	\begin{multicols}{2}
+	   \begin{itemize}
+		\item \CSharp
+		\item ExtJS
+		\item LINQ\textbackslash SQL
+		\item ASP.NET
+		\item TFS
+		\item RDL/RDLC
+	   \end{itemize}
+	\end{multicols}
+	 \vspace{-3mm} 
+\item \textbf{Accomplishments:}
+  \begin{itemize}
+  \item Main developer for several projects including SAHARA clinical trial
+  \item Built public website for assessing heart risk score \url{https://rome.phri.ca/interheartriskscore}
+  \item Found security flaws, such as SQL injections, fixed flaws and led redeployment of project
+  \item Assessed new technologies and alternatives to existing systems (such as mobile and SMS service)
+  \end{itemize}
+\end{itemize}}
+*/
