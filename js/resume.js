6e952beab34024f39f421f71b39e01174b29a4a8
mirhagk <mirhagk@gmail.com>
2013-10-04 10:13:47 -0400

    Separated blog and resume data into own files

---
+++ js/resume.js
@@ -0,0 +1,8 @@
+var experience = [{
+    name: 'Population Health Research Institute',
+    location: 'Hamilton, ON',
+    title: 'Software Developer',
+    tech: [{name: 'C#'},{name: 'ExtJS'}],
+    respon: [{text: ''}],
+    achiev: [{text: ''}]
+}];
