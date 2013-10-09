43247ac834ba644008b4cd24311b9a9f45e4555f
mirhagk <mirhagk@gmail.com>
2013-10-09 01:28:36 -0400

    Added autogenresume node script
    It will use a template resume and a javascript file to generate a
    formatted resume with the content.

---
+++ autogenresume.js
@@ -0,0 +1,46 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+require('./website/js/resume.js');
+function puts(error, stdout, stderr) { console.log(stdout) }
+//exec("dir", puts);
+
+function FormatTemplate(template, object){
+    var result = template;
+    var eachRegex = /{{#each ?([^{}]*)}}([\s\S]*?){{\/each}}/g;
+    var each = eachRegex.exec(result);
+    while(each!=null){
+        console.log('found');
+        var array = object[each[1]];
+        var section = "";
+        for(var i = 0;i<array.length;i++){
+            section+=FormatTemplate(each[2],array[i]);
+        }
+        result = result.substring(0,each.index)+
+                    section+
+                   template.substring(each.index+each[0].length,result.length);
+        console.log(section);
+        each = eachRegex.exec(result);
+    }
+    var matches = template.match(/{{[^#/{}][^{}]*}}/g);
+    if (matches!=null){
+        for(var i=0;i<matches.length;i++){
+            var key = matches[i].substring(2,matches[i].length-2);
+            console.log(key);
+            if (object[key]!=null)
+                result = result.replace('{{'+key+'}}',object[key]);
+        }
+    }
+    //for(var i=0;i<each.length;i++){
+        //console.log(each[i]);
+    //}
+    //console.log(each[0]);
+    //console.log(each[1]);
+    console.log('-------');
+    return result;
+}
+
+var fs = require('fs');
+var contents = fs.readFileSync('template/test.txt').toString();
+var resume = {thing: 'stuff', test: 'things'}
+contents = FormatTemplate(contents,resumeData);
+console.log(contents);
