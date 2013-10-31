4895951e7f35d5b559eb3d12a6e8b9b39879120b
Nathan Jervis <mirhagk@gmail.com>
2013-10-31 11:40:20 -0400

    Finished tokenizer
    The input stream is not split up into tokens. A quick "parser" will be
    required to turn it into a tree-like situation, matching up each's but
    we're vey close to the end now

--- autogenresume.js
+++ autogenresume.js
@@ -5,6 +5,13 @@ require('./website/js/resume.js');
 function puts(error, stdout, stderr) { console.log(stdout) }
 //exec("dir", puts);
 
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
+
 //Splits the input into tokens, basically either text or hanlebar sections
 function Tokenizer(text){
 	var handlebar = /{{[^{}]*}}/;
@@ -15,13 +22,44 @@ function Tokenizer(text){
 	console.log(next.index);
 	console.log('=============');
 	while(next!=null){
-		tokens.push(text.substr(0,next.index));
-		tokens.push(next[0]);
+		tokens.push({
+			type: "content", 
+			text: text.substr(0,next.index)
+		});
+		var token = next[0];
+		token = token.substr(2,token.length-4);
+		console.log(token);
+		switch(token[0])
+		{
+			case '#':
+				if (token.startsWith("#each "))
+					tokens.push({
+						type: "eachBegin",
+						text: token.substr("#each ".length)
+					});
+				break;
+			case '/':
+				if (token.startsWith("/each"))
+					tokens.push({
+						type: "eachEnd"
+					});
+				break;
+			default:
+				tokens.push({
+					type: "replacement",
+					text: token
+				});
+				break;
+		}
+		//tokens.push(next[0]);
 		text = text.substr(next.index+next[0].length);
 		next = handlebar.exec(text);
 	}
 	if (text.length!=0)
-		tokens.push({type: "content", text: text});
+		tokens.push({
+			type: "content", 
+			text: text
+		});
 	return tokens;
 }
 
