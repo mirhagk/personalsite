4ef1601c8679234795d965eb7b91be5bbf872f13
Nathan Jervis <mirhagk@gmail.com>
2013-10-31 12:00:45 -0400

    Removed comments and old code
    Just general clean up of code and commented out code

--- resumeGenerator/autogenresume.js
+++ resumeGenerator/autogenresume.js
@@ -3,56 +3,8 @@ var exec = require('child_process').exec;
 require('./lib.js');
 var parser = require('./parser.js');
 require('../website/js/resume.js');
-//var wait = require('wait.for');
-//exec("dir", puts);
 
-function GetEachTags(text){
-    var eachStart = /{{#each ?([^{}]*)}}/g;
-    var each = eachStart.exec(text);
-}
-
-function FormatTemplate(template, object){
-    var result = template;
-    var eachRegex = /{{#each ?([^{}]*)}}([\s\S]*?){{\/each}}/g;
-    var each = eachRegex.exec(result);
-    while(each!=null){
-        var array = object[each[1]];
-        var section = "";
-        for(var i = 0;i<array.length;i++){
-            section+=FormatTemplate(each[2],array[i]);
-        }
-        result = result.substring(0,each.index)+
-                    section+
-                   template.substring(each.index+each[0].length,result.length);
-        console.log(section);
-        each = eachRegex.exec(result);
-    }
-    var matches = template.match(/{{[^#/{}][^{}]*}}/g);
-    if (matches!=null){
-        for(var i=0;i<matches.length;i++){
-            var originalKey = matches[i].substring(2,matches[i].length-2);
-            var key = originalKey.substring(0,originalKey.length);
-            var obj = object;
-            var kip = key.indexOf('.');
-            while (kip>-1){
-                if (obj!=null)
-                    obj =obj[key.substring(0,kip)];
-                key = key.substring(kip+1,key.length);
-                kip=key.indexOf('.');
-            }
-            if (obj!=null && obj[key]!=null){
-                result = result.replace('{{'+originalKey+'}}',obj[key]);
-            }
-        }
-    }
-    console.log('-------');
-    return result;
-}
-
-console.log(parser);
 var fs = require('fs');
 var contents = fs.readFileSync('../template/resume.tex').toString();
 var tokens = parser.tokenizer(contents);
 console.log(tokens);
-//contents = FormatTemplate(contents,resumeData);
-//console.log(contents);
