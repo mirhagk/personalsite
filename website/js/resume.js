9aaab9195333b2f95f35239e5a1e95c5806f002d
Nathan Jervis <mirhagk@gmail.com>
2013-11-04 12:18:20 -0500

    Completed experience section
    Also only show accomplishments if there are any

--- website/js/resume.js
+++ website/js/resume.js
@@ -21,7 +21,7 @@ var experience = [{
         ],
     accom: [
         {text: 'Main developer for several projects including SAHARA clinical trial'},
-        {text: 'Built public website for assessing heart risk score https://rome.phri.ca/interheartriskscore'},
+        {text: 'Built public website for assessing heart risk score <a href="https://rome.phri.ca/interheartriskscore">https://rome.phri.ca/interheartriskscore</a>'},
         {text: 'Found security flaws, such as SQL injections, fixed flaws and led redeployment of project'},
         {text: 'Assessed new technologies and alternatives to existing systems (such as mobile and SMS service)'}
         ]
@@ -48,7 +48,48 @@ var experience = [{
 		{text: 'Build a server that will scale easily, and allow for rapid future changes'}
 		],
 	accom: [
-		{text: 'Built tool to compress javascript, compile less code, zip code into package and upload package to http://build.phonegap.com to build mobile apps'}
+		{text: 'Built tool to compress javascript, compile less code, zip code into package and upload package to <a href="http://build.phonegap.com">http://build.phonegap.com</a> to build mobile apps'}
+		]
+},{
+	name: 'NVC Software Solutions',
+	description: 'Created a ran a software contracting company. Served several clients building websites, applications and more.',
+	title: 'President and Lead Programmer',
+    location: 'Hamilton, ON',
+    dates: {start: 'March 2013', end: 'September 2013'},
+	tech: [
+		{name: 'C#'},
+		{name: 'Java'},
+		{name: 'SQL'},
+		{name: 'Python'},
+		{name: 'HTML/CSS'},
+		{name: 'Javascript/JQuery'},
+		{name: 'Bootstrap'}
+		],
+	respon: [
+		{text: 'Attract clients and negotiate contracts'},
+		{text: 'Manage a team of developers, delegating work, training and reviewing code'},
+		{text: 'Report all finances and make spending decisions'}
+		],
+	accom: [
+		{text:'Build a bot to crawl and analyze local websites and assess whether a website redesign could be useful automatically. Based on several factors including html tag usage'}
+		]
+},{
+	name: 'Rendezvous Mobile',
+	description: 'Design, architect and create backend for mobile application. Mobile application details are still confidential.',
+	title: 'Lead Backend Programmer',
+    location: 'Hamilton, ON',
+    dates: {start: 'October 2013', end: 'present'},
+	tech: [
+		{name: 'C#'},
+		{name: 'ASP.NET'},
+		{name: 'Azure Cloud Services'},
+		{name: 'SQL/LINQ'},
+		{name: 'JSON'}
+		],
+	respon: [
+		{text: 'Design a protocol for the client application to communicate with the server'},
+		{text: 'Draft specification documents and database layout'},
+		{text: 'Construct database and API according to specification'}
 		]
 }];
 resumeData = {name: 'Nathan Jervis', experience: experience}
