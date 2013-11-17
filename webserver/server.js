93f5264f0bdb8fb00d283ff45e39105f21de2e73
Nathan Jervis <mirhagk@gmail.com>
2013-11-16 22:53:20 -0500

    removed comments, adding logging of errors

--- webserver/server.js
+++ webserver/server.js
@@ -2,11 +2,11 @@ var static = require('node-static');
 var port = process.env.port || 1337;
 var http = require('http');
 var fs = require('fs');
+var util = require('util');
 
 
 const templateFolder = 'template/';
 var fileServer = new static.Server('./www');
-//var templateServer = new static.Server('./template');
 
 
 function CreateSite(path, res){
@@ -14,7 +14,6 @@ function CreateSite(path, res){
 	for(var i=0;i<files.length;i++){
 		if (files[i].match(/\.handlebars$/g)){//handlebar file
 			var file = templateFolder+'pages/'+path+files[i];
-			console.log(file);
 			var part = '<script type="text/x-handlebars" id="'+path+files[i].replace(/\.handlebars$/,"")+'">\n' 
 						+ fs.readFileSync(file)
 						+ '\n</script>\n';
@@ -23,34 +22,19 @@ function CreateSite(path, res){
 		else if (files[i].match(/^[^\.]*$/g)){//folder
 			CreateSite(path+files[i]+'/',res);
 		}
+		else{//we have an issue here, log it
+			util.error('Found '+path+files[i]+'which isn\'t a handlebar file or folder');
+		}
 	}
 }
 
 http.createServer(function (req, res) {
-	//console.log(req.url);
 	if (req.url == '/' || req.url == '/index.html'){
 		var files = fs.readdirSync(templateFolder+'pages');
-		console.log(files);
 		res.writeHead(200);
 		res.write(fs.readFileSync(templateFolder+'start.html'));
 		CreateSite('',res);
 		res.end(fs.readFileSync(templateFolder+'end.html'));
-		//fs.readFileSync(templateFolder+'index.html');
-		//for(var x=0;x<files.length;x++)
-		//{
-		//}
-		/*fs.readFile(templateFolder+'index.html',function(err,data){
-			if (err){
-				res.writeHead(404);
-				//res.end(JSON.stringify(err));//TODO: Log this somewhere
-				res.end('Bad things happened');
-				return;
-			}
-			res.writeHead(200);
-			res.end(data);
-		});*/
-		//console.log('main code');
-		//templateServer.serveFile('/index.html',200,{},req,res);
 	}
 	else
 		fileServer.serve(req, res);
