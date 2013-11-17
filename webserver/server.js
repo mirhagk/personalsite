786144bb3d12730c98985b4dc75b29172828ac24
Nathan Jervis <mirhagk@gmail.com>
2013-11-16 21:39:30 -0500

    Removing console logging and now watching port
    Needs to connect to process.env.port if it exists

--- webserver/server.js
+++ webserver/server.js
@@ -1,15 +1,16 @@
 var static = require('node-static');
+var port = process.env.port || 1337;
 var http = require('http');
 
 var fileServer = new static.Server('./www');
 var templateServer = new static.Server('./template');
 
 http.createServer(function (req, res) {
-	console.log(req.url);
+	//console.log(req.url);
 	if (req.url == '/' || req.url == '/index.html'){
-		console.log('main code');
+		//console.log('main code');
 		templateServer.serveFile('/index.html',200,{},req,res);
 	}
 	else
 		fileServer.serve(req, res);
-}).listen(80);
+}).listen(port);
