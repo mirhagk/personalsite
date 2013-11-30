d70062df3d97bdfac7ed9b96d213b082550d7c20
Nathan Jervis <mirhagk@gmail.com>
2013-11-30 16:24:53 -0500

    Added Sublime project and other pages
    Now with sublime text can rebuild the pages. Also has all the required
    pages now

--- website/build.js
+++ website/build.js
@@ -6,19 +6,17 @@ bliss = new Bliss();
 function Title(file){
 	return capitaliseFirstLetter(file.match(/[^\.]*/)[0]);
 }
-function capitaliseFirstLetter(string)
-{
+function capitaliseFirstLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-template = bliss.compileFile('src/layout');
-var pages = fs.readdirSync('src/pages');
-console.log(pages);
-//var pages = ["home","about"];
+template = bliss.compileFile(__dirname+'/src/layout');
+var pages = fs.readdirSync(__dirname+'/src/pages');
+console.log('found pages: ', pages);
+
 pages.forEach(function(page){
 	var things = template(Title(page),function(){
-		return bliss.render("src/pages/"+page);
+		return bliss.render(__dirname+"/src/pages/"+page);
 	});
-	console.log(things);
-	fs.writeFileSync('www/'+Title(page)+'.html',things);
+	fs.writeFileSync(__dirname+'/www/'+Title(page)+'.html',things);
 });
