235d1367a0f49bf56b12d5586e48b5858a64801d
Nathan Jervis <mirhagk@gmail.com>
2013-12-09 22:49:16 -0500

    Added robots.txt
    Will load robots.txt correctly now.

--- webserver/webserver/App_Start/RouteConfig.cs
+++ webserver/webserver/App_Start/RouteConfig.cs
@@ -13,11 +13,18 @@ namespace webserver
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Robots.txt",
+                "robots.txt",
+                new { controller = "Meta", action = "Robots" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute("Sitemap.xml",
+                "sitemap.xml",
+                new { controller = "Meta", action = "Sitemap" });
         }
     }
 }
---
+++ webserver/webserver/Controllers/MetaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webserver.Controllers
+{
+    public class MetaController : Controller
+    {
+        //
+        // GET: /Meta/
+
+        public ActionResult Robots()
+        {
+            Response.ContentType = "text/plain";
+            return View();
+        }
+
+    }
+}
