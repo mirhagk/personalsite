13beae54fbd3c19b700b75beda85b16ee580c3e6
Nathan Jervis <mirhagk@gmail.com>
2013-12-14 15:36:30 -0500

    Added self test and crawl pages

--- webserver/webserver/Controllers/MetaController.cs
+++ webserver/webserver/Controllers/MetaController.cs
@@ -21,5 +21,56 @@ namespace webserver.Controllers
             Response.ContentType = "application/xml";
             return View();
         }
+        private IEnumerable<string> GetAllLinksForPage(string url)
+        {
+            try
+            {
+                string root = "http://" + new System.Uri(url).Host;
+                var client = new System.Net.WebClient();
+                CsQuery.CQ data = client.DownloadString(url);
+                return data["a"].Select((x) => x["href"]).Where((x) => x.StartsWith("/")).Select((x) => root + x);
+            }
+            catch
+            {
+                return new List<string>(0);
+            }
+        }
+        private List<string> GetAllLinksForSite(string root)
+        {
+            List<string> links = new List<string>();
+            links.AddRange(GetAllLinksForPage(root));
+            links = links.Distinct().ToList();
+            for (int i = 0; i < links.Count; i++)
+            {
+                links.AddRange(GetAllLinksForPage(links[i]));
+                links = links.Distinct().ToList();
+            }
+            return links;
+        }
+        public ActionResult CrawlSite(string root)
+        {
+            var result = new ContentResult();
+            result.Content = "sucess";
+
+            result.Content = string.Join("<br />\n", GetAllLinksForSite(root));
+            return result;
+        }
+        public ActionResult SelfTest(string root)
+        {
+            List<string> pages = GetAllLinksForSite(root);
+            var client = new System.Net.WebClient();
+            try
+            {
+                foreach (var page in pages)
+                {
+                    client.DownloadString(page);
+                }
+                return new ContentResult() { Content = "Success" };
+            }
+            catch
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+        }
     }
 }
